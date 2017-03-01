a68ab133c8e9da3d7fd25d5efbdf6b28d7af32f4
Simon Tong <simon.tong@yoti.com>
2017-03-01 15:46:09 +0000

    add id field

--- yoti_connect/YotiConnectHelper.php
+++ yoti_connect/YotiConnectHelper.php
@@ -39,9 +39,6 @@ class YotiConnectHelper
      */
     public function link()
     {
-        global $user;
-
-        $currentUser = $user;
         $config = self::getConfig();
         $token = (!empty($_GET['token'])) ? $_GET['token'] : null;
 
@@ -76,77 +73,18 @@ class YotiConnectHelper
         }
 
         // check if yoti user exists
-        $userId = $this->getUserIdByYotiId($activityDetails->getUserId());
-
-        // if yoti user exists in db but isn't an actual account then remove it from yoti table
-        if ($userId && $currentUser && $currentUser->uid != $userId && !user_load($userId))
+        $exists = $this->getUserIdByYotiId($activityDetails->getUserId());
+        if ($exists)
         {
-            // remove users account
-            $this->deleteYotiUser($userId);
-        }
-
-        // if user isn't logged in
-        if (!$currentUser->uid)
-        {
-            // register new user
-            if (!$userId)
-            {
-                $errMsg = $userId = null;
-                try
-                {
-                    $userId = $this->createUser($activityDetails);
-                }
-                catch (Exception $e)
-                {
-                    $errMsg = $e->getMessage();
-                }
-
-                // no user id? no account
-                if (!$userId)
-                {
-                    // if couldn't create user then bail
-                    $this->setFlash("Could not create user account. $errMsg", 'error');
-
-                    return false;
-                }
-            }
-
-            // log user in
-            $this->loginUser($userId);
+            $this->setFlash('Yoti user already stored in database.', 'error');
         }
         else
         {
-            // if current logged in user doesn't match yoti user registered then bail
-            if ($userId && $currentUser->uid != $userId)
-            {
-                $this->setFlash('This Yoti account is already linked to another account.', 'error');
-            }
-            // if joomla user not found in yoti table then create new yoti user
-            elseif (!$userId)
-            {
-                $this->createYotiUser($currentUser->uid, $activityDetails);
-                $this->setFlash('Your Yoti account has been successfully linked.');
-            }
+            $this->createYotiUser($activityDetails);
+            $this->setFlash('Yoti account stored successfully.');
         }
 
         return true;
-    }
-
-    /**
-     * Unlink account from currently logged in
-     */
-    public function unlink()
-    {
-        global $user;
-
-        // unlink
-        if ($user)
-        {
-            $this->deleteYotiUser($user->uid);
-            return true;
-        }
-
-        return false;
     }
 
     /**
@@ -159,105 +97,20 @@ class YotiConnectHelper
     }
 
     /**
-     * @param string $prefix
-     * @return string
-     */
-    private function generateUsername($prefix = 'yoticonnect-')
-    {
-        // generate username
-        $i = 0;
-        do
-        {
-            $username = $prefix . $i++;
-        }
-        while (user_load_by_name($username));
-
-        return $username;
-    }
-
-    /**
-     * @param $prefix
-     * @param string $domain
-     * @return string
-     */
-    private function generateEmail($prefix = 'yoticonnect-', $domain = 'example.com')
-    {
-        // generate email
-        $i = 0;
-        do
-        {
-            $email = $prefix . $i++ . "@$domain";
-        }
-        while (user_load_by_mail($email));
-
-        return $email;
-    }
-
-    /**
-     * @param int $length
-     * @return string
-     */
-    private function generatePassword($length = 10)
-    {
-        // generate password
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $password = ''; //remember to declare $pass as an array
-        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < $length; $i++)
-        {
-            $n = rand(0, $alphaLength);
-            $password .= $alphabet[$n];
-        }
-
-        return $password;
-    }
-
-    /**
-     * @param ActivityDetails $activityDetails
-     * @return int
-     * @throws Exception
-     */
-    private function createUser(ActivityDetails $activityDetails)
-    {
-        $user = array(
-            'status' => 1,
-            //            'roles' => array(
-            //                DRUPAL_AUTHENTICATED_RID => 'authenticated user',
-            //                3 => 'custom role',
-            //            ),
-        );
-
-        //Mandatory settings
-        $user['pass'] = $this->generatePassword();
-        $user['mail'] = $user['init'] = $this->generateEmail();
-        $user['name'] = $this->generateUsername();//This username must be unique and accept only a-Z,0-9, - _ @ .
-
-        // The first parameter is sent blank so a new user is created.
-        $user = user_save('', $user);
-
-        // set new id
-        $userId = $user->uid;
-        $this->createYotiUser($userId, $activityDetails);
-
-        return $userId;
-    }
-
-    /**
      * @param $yotiId
      * @return int
      */
     private function getUserIdByYotiId($yotiId)
     {
         $tableName = self::tableName();
-        $col = db_query("SELECT uid FROM `{$tableName}` WHERE identifier = '$yotiId'")->fetchCol();
+        $col = db_query("SELECT identifier FROM `{$tableName}` WHERE identifier = '$yotiId'")->fetchCol();
         return ($col) ? reset($col) : null;
     }
 
     /**
-     * @param $userId
      * @param ActivityDetails $activityDetails
      */
-    private function createYotiUser($userId, ActivityDetails $activityDetails)
+    private function createYotiUser(ActivityDetails $activityDetails)
     {
         //        $user = user_load($userId);
         $meta = $activityDetails->getProfileAttribute();
@@ -272,7 +125,7 @@ class YotiConnectHelper
                 drupal_mkdir($uploadDir, 0777, true);
             }
 
-            $selfieFilename = md5("selfie_$userId" . time()) . ".png";
+            $selfieFilename = md5("selfie" . time()) . ".png";
             file_put_contents("$uploadDir/$selfieFilename", $content);
             //      file_put_contents(self::uploadDir() . "/$selfieFilename", $activityDetails->getUserProfile('selfie'));
             $meta['selfie_filename'] = $selfieFilename;
@@ -285,7 +138,7 @@ class YotiConnectHelper
         //unset($meta[ActivityDetails::ATTR_SELFIE]); // don't save selfie to db
 
         db_insert(self::tableName())->fields(array(
-            'uid' => $userId,
+//            'uid' => $userId,
             'identifier' => $activityDetails->getUserId(),
             'phone_number' => $activityDetails->getProfileAttribute(ActivityDetails::ATTR_PHONE_NUMBER),
             'date_of_birth' => $activityDetails->getProfileAttribute(ActivityDetails::ATTR_DATE_OF_BIRTH),
@@ -300,32 +153,12 @@ class YotiConnectHelper
     }
 
     /**
-     * @param int $userId joomla user id
-     */
-    private function deleteYotiUser($userId)
-    {
-        db_delete(self::tableName())->condition("uid", $userId)->execute();
-    }
-
-    /**
-     * @param $userId
-     */
-    private function loginUser($userId)
-    {
-        //        $user = user_load($userId);
-        //        var_dump($user);exit;
-        //        user_login_finalize($user);
-        $form_state['uid'] = $userId;
-        user_login_submit(array(), $form_state);
-    }
-
-    /**
      * not used in this instance
      * @return string
      */
     public static function tableName()
     {
-        return 'users_yoti';
+        return 'users_yoti_civi';
     }
 
     /**
